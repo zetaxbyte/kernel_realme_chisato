7e36ad48f0ad66ec8eb93b4f2e99039a0e5b89cb
Ingo Molnar <mingo@kernel.org>
2022-10-27 13:54:29 -0700

    x86/cpufeature: Fix various quality problems in the <asm/cpu_device_hd.h> header
    commit 266d63a7d9d48c6d5dee486378ec0e8c86c4d74a upstream
    
    Thomas noticed that the new arch/x86/include/asm/cpu_device_id.h header is
    a train-wreck that didn't incorporate review feedback like not using __u8
    in kernel-only headers.
    
    While at it also fix all the *other* problems this header has:
    
     - Use canonical names for the header guards. It's inexplicable why a non-standard
       guard was used.
    
     - Don't define the header guard to 1. Plus annotate the closing #endif as done
       absolutely every other header. Again, an inexplicable source of noise.
    
     - Move the kernel API calls provided by this header next to each other, there's
       absolutely no reason to have them spread apart in the header.
    
     - Align the INTEL_CPU_DESC() macro initializations vertically, this is easier to
       read and it's also the canonical style.
    
     - Actually name the macro arguments properly: instead of 'mod, step, rev',
       spell out 'model, stepping, revision' - it's not like we have a lack of
       characters in this header.
    
     - Actually make arguments macro-safe - again it's inexplicable why it wasn't
       done properly to begin with.
    
    Quite amazing how many problems a 41 lines header can contain.
    
    This kind of code quality is unacceptable, and it slipped through the
    review net of 2 developers and 2 maintainers, including myself, until
    Thomas noticed it. :-/
    
    Reported-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/include/asm/cpu_device_id.h
+++ arch/x86/include/asm/cpu_device_id.h
@@ -184,21 +184,22 @@ extern bool x86_cpu_has_min_microcode_rev(const struct x86_cpu_desc *table);
  */
 
 struct x86_cpu_desc {
-	__u8	x86_family;
-	__u8	x86_vendor;
-	__u8	x86_model;
-	__u8	x86_stepping;
-	__u32	x86_microcode_rev;
+	u8	x86_family;
+	u8	x86_vendor;
+	u8	x86_model;
+	u8	x86_stepping;
+	u32	x86_microcode_rev;
 };
 
-#define INTEL_CPU_DESC(mod, step, rev) {			\
-	.x86_family = 6,					\
-	.x86_vendor = X86_VENDOR_INTEL,				\
-	.x86_model = mod,					\
-	.x86_stepping = step,					\
-	.x86_microcode_rev = rev,				\
+#define INTEL_CPU_DESC(model, stepping, revision) {		\
+	.x86_family		= 6,				\
+	.x86_vendor		= X86_VENDOR_INTEL,		\
+	.x86_model		= (model),			\
+	.x86_stepping		= (stepping),			\
+	.x86_microcode_rev	= (revision),			\
 }
 
+extern const struct x86_cpu_id *x86_match_cpu(const struct x86_cpu_id *match);
 extern bool x86_cpu_has_min_microcode_rev(const struct x86_cpu_desc *table);
 
-#endif
+#endif /* _ASM_X86_CPU_DEVICE_ID */
